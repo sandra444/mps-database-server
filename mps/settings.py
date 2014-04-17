6ec8c5bc1a0b703fe8d021079826d1ee3a895750
Tongying Shun <tos8@pitt.edu>
2014-04-17 17:49:18 -0400

    Configure MPS static assets

--- mps/settings.py
+++ mps/settings.py
@@ -110,7 +110,7 @@ MEDIA_ROOT = os.path.join(PROJECT_ROOT, '..', MEDIA_URL.strip("/"))
 # Additional locations of static files
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
-    os.path.join(PROJECT_ROOT, '../angularjs/'),
+    os.path.join(PROJECT_ROOT, '../../mps-web-client/'),
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
--- mps/views.py
+++ mps/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
-
+import os
+import settings
 
 def main(request):
-    return HttpResponse(open("angularjs/mps.html"))
+    return HttpResponse(open(os.path.join(settings.STATIC_ROOT, 'mps.html')))
