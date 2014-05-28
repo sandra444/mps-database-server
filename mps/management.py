6e8427c44d2093c62360827d6534f96623dbf7e9
Adam Gradzki <adam.gradzki@gmail.com>
2014-05-28 17:45:39 -0400

    Whitelist requests from Github

--- mps/management.py
+++ mps/management.py
@@ -9,11 +9,18 @@ from django.views.decorators.csrf import csrf_exempt
 def webhook(request):
     try:
         data = json.loads(request.body)
-    except ValueError:
-        return HttpResponse(status=405)
-    try:
-        print request.META['REMOTE_ADDR']
+        source_ip = request.META['REMOTE_ADDR'].split('.')
+
+        # if the request isn't from Github, forget it.
+        if ('192' not in source_ip[0]) \
+                and ('30' not in source_ip[1]) \
+                and ('252' not in source_ip[2]):
+            raise ValueError
+
+        # we only want to update if we push to master
         if "refs/heads/master" in data['ref']:
+
+            # adam is responsible for deploying to master
             if "nszceta" in data['pusher']['name']:
                 subprocess.call(['git', 'reset', '--hard', 'HEAD'])
                 subprocess.call(['git', 'fetch'])
@@ -21,8 +28,13 @@ def webhook(request):
                 subprocess.call(['git', 'reset', '--hard', 'HEAD'])
                 subprocess.call(['touch', '/home/mps/touch-reload-production'])
                 return HttpResponse(status=200)
+
     except KeyError:
         pass
+    except ValueError:
+        pass
+
+    # if anything goes wrong, return 'method not allowed' code 405
     return HttpResponse(status=405)
 
 
