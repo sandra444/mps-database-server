97a3045d6279be848dbd91a78c7a8c7909af41cc
Adam Gradzki <adam.gradzki@gmail.com>
2014-05-28 16:27:27 -0400

    Reload server on Github webhook call

--- mps/management.py
+++ mps/management.py
@@ -1,15 +1,23 @@
 import json
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+import subprocess
 
 
+@csrf_exempt
 def webhook(request):
-    if "refs/heads/master" in request.POST.ref:
-        data = {'status': 'ok'}
-    else:
-        data = {'status': 'bad'}
-    return HttpResponse(json.dumps(data), content_type="application/json")
+    data = json.loads(request.body)
+
+    try:
+        if "refs/heads/master" in data.ref:
+            if "nszceta" in data.pusher.name:
+                subprocess.call(["touch", "~/touch-reload-production"])
+                return HttpResponse(status=200)
+    except KeyError:
+        return HttpResponse(status=405)
 
 
+@csrf_exempt
 def database(request):
     data = {'status': 'ok'}
     return HttpResponse(json.dumps(data), content_type="application/json")
