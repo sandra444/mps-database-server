16e7b96f8578a9166e840e8c964fb5570ce80e35
Adam Gradzki <adam.gradzki@gmail.com>
2014-05-28 15:39:25 -0400

    Check ref in web hook

--- mps/management.py
+++ mps/management.py
@@ -3,10 +3,13 @@ from django.http import HttpResponse
 
 
 def webhook(request):
-    pass
+    if "refs/heads/master" in request.POST.ref:
+        data = {'status': 'ok'}
+    else:
+        data = {'status': 'bad'}
+    return HttpResponse(json.dumps(data), content_type="application/json")
 
 
 def database(request):
-
     data = {'status': 'ok'}
     return HttpResponse(json.dumps(data), content_type="application/json")
