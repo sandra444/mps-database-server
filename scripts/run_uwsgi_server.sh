a9bde3dc7889add9b51919b4191e0c1b7bad17a1
nszceta <adam.gradzki@gmail.com>
2014-06-05 06:20:16 -0400

    Modify uwsgi server timeouts

--- scripts/run_uwsgi_server.sh
+++ scripts/run_uwsgi_server.sh
@@ -4,9 +4,9 @@ uwsgi --chdir=/home/mps/mps-database-server \
  --module=mps.wsgi:application  \
  --env DJANGO_SETTINGS_MODULE=mps.settings \
  --socket=127.0.0.1:8090 \
- --processes=32 \
- --harakiri=100 \
- --max-requests=3000 \
+ --processes=16 \
+ --harakiri=300 \
+ --max-requests=9000 \
  --vacuum \
  --daemonize=/home/mps/log/mps.log \
  --threads 2 \
