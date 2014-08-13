38c376892e2f720792ced5685ddb3a0a3d020195
tputinsky <tessputinsky@gmail.com>
2014-08-13 10:01:00 -0400

    Added javascript function to change row labels input box on change, not sure if works

--- assays/static/assays/extra_button.js
+++ assays/static/assays/extra_button.js
@@ -1,3 +1,26 @@
-/**
- * Created by developer on 8/13/14.
- */
+$(document).ready(function () {
+
+    var row_field = document.getElementById('id_row_labels');
+    var array = [row_field];
+    console.log(array);
+
+    row_field.change(function() {
+        if ( row_field.value.split().length == 1 ){
+            var start = row_field.value;
+            var rows = document.getElementById('id_number_of_rows').value;
+
+            if ( rows == undefined ){
+            }
+            else {
+
+                var input = "";
+                for ( var i = start; i <= rows; i++){
+                    input += i + " ";
+                }
+                row_field.value(input);
+
+            }
+        }
+    }, false);
+
+});
