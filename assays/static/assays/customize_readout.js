93e9e0b04e89ad82b927c3c941e049c3c0c03161
tputinsky <tessputinsky@gmail.com>
2014-08-14 10:23:40 -0400

    attempting to make function which will automatically load assay layout table in assay device readout

--- assays/static/assays/customize_readout.js
+++ assays/static/assays/customize_readout.js
@@ -301,11 +301,11 @@ $(document).ready(function () {
         // Refer to the benchmarks for more information:
         // http://jsperf.com/performance-of-parseint
 
-        try {
-            return Math.floor($('.historylink').attr('href').split('/')[4]);
-        }catch(err){
-            return 0;
-        }
+        //not sure this is working before save happens, could be causing issue
+        //this is undefined when picking assay layout
+
+        return Math.floor($('.historylink').attr('href').split('/')[4]);
+
     }
 
     function checkAssayLayoutValidity() {
