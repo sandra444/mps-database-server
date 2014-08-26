99ba9bc291ef94f3728a05d499aa8645411818d1
Adam Gradzki <adam.gradzki@gmail.com>
2014-08-26 09:53:56 -0400

    Fix broken settings and incorrectly placed braces

--- mps/settings.py
+++ mps/settings.py
@@ -203,8 +203,8 @@ LOGGING = {
     'disable_existing_loggers': False,
     'formatters': {
         'verbose': {
-            'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
-            'datefmt' : "%d/%b/%Y %H:%M:%S"
+            'format': "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt': "%d/%b/%Y %H:%M:%S"
         },
         'simple': {
             'format': '%(levelname)s %(message)s'
@@ -222,9 +222,9 @@ LOGGING = {
     'loggers': {
 
         'django': {
-            'handlers':['file'],
+            'handlers': ['file'],
             'propagate': True,
-            'level':'DEBUG',
+            'level': 'DEBUG',
         },
 
         'assays': {
@@ -262,7 +262,6 @@ LOGGING = {
             'level': 'DEBUG',
         },
 
-        },
     }
 }
 
