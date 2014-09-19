a4f26905e9fec3ad4dbc307810e44aaed41ada86
Luke-Malchow-Bergenthal <lmb140@pitt.edu>
2014-09-19 18:37:04 -0400

    More or less functional initial table; now implement pre-existing table

--- assays/static/assays/customize_chip.js
+++ assays/static/assays/customize_chip.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     $('<div id="csv_table" align="center" style="margin-top: 10px;margin-bottom: 10px;">').appendTo('body').html("<table class='layout-table' style='width: 99%;'><tbody><tr><th>Time</th><th>Field</th><th>Raw Data</th></tr><tr><th><br><br></th><th><br><br></th><th><br><br></th></tr><tr><th><br><br></th><th><br><br></th><th><br><br></th></tr></tbody></table>");
-    $( "#csv_table" ).insertBefore( $( ".module" )[3]);
+    $("#csv_table").insertBefore($(".module")[3]);
     
     $('#id_file').change(function(evt) {
         console.log("A change has been registered");
@@ -19,10 +19,34 @@ $(document).ready(function () {
     var loaded = function (evt) {
         var fileString = evt.target.result;
         console.log(fileString);
+        parseAndReplace(fileString);
     }
     
     var parseAndReplace = function (csv) {
+        var all = csv.split('\n');
+        var lines = [];
         
+        for (i in all) {
+            lines.push(all[i].split(','));
+        }
+        
+        all = null;
+        
+        table = "<table class='layout-table' style='width: 99%;'><tbody>";
+        
+        for (i in lines) {
+            console.log(lines[i]);
+            table += "<tr>";
+            table += "<th><br>" + lines[i][0] + "<br></th>";
+            table += "<th><br>" + lines[i][1] + "<br></th>";
+            table += "<th><br>" + lines[i][2] + "<br></th>";
+            table += "</tr>";
+        }
+        
+        table += "</tbody></table>";
+        console.log(table);
+        
+        $('#csv_table').html(table);
     }   
     
 });
