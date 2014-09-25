96bf58505e8506c984fee49d9e352a9bff3f953d
Luke-Malchow-Bergenthal <lmb140@pitt.edu>
2014-09-25 17:03:09 -0400

    Initial JS and minor edit to customize_chip

---
+++ assays/static/assays/customize_chip_results_admin.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+
+    var middleware_token = $('[name=csrfmiddlewaretoken]').attr('value');
+
+    $('#id_assay_device_readout').change(function(evt) {
+        
+    });
+});
