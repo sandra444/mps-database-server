1121a1e29f1ecca8dd40c090f04e7c65d84e930c
Luke-Malchow-Bergenthal <lmb140@pitt.edu>
2014-10-16 13:36:24 -0400

    Test model to form

--- assays/urls.py
+++ assays/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, url
-from assays.views import RunList
+from assays.views import RunList, manage_readouts
 
 urlpatterns = patterns('',
     url(r'^assays/runs/$', RunList.as_view()),
+    url(r'^assays/read/$', manage_readouts),
 )
