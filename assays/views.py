ba22ec7da451246c8bae0db668b3b4996129dbd1
Luke-Malchow-Bergenthal <lmb140@pitt.edu>
2014-10-16 13:36:24 -0400

    Test model to form

--- assays/views.py
+++ assays/views.py
@@ -3,3 +3,20 @@ from assays.models import AssayRun
 
 class RunList(ListView):
     model = AssayRun
+
+from django.forms.models import modelformset_factory
+from django.shortcuts import render_to_response
+from assays.models import AssayChipReadout
+
+def manage_readouts(request):
+    ReadoutFormSet = modelformset_factory(AssayChipReadout)
+    if request.method == 'POST':
+        formset = ReadoutFormSet(request.POST, request.FILES)
+        if formset.is_valid():
+            formset.save()
+            # do something.
+    else:
+        formset = ReadoutFormSet()
+    return render_to_response("assays/manage_readouts.html", {
+        "formset": formset,
+    })
