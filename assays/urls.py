b305417560c348392f18fd1b2fb70bd984a5c1a5
Luke-Malchow-Bergenthal <lmb140@pitt.edu>
2014-10-17 15:08:50 -0400

    Change names and layout of add

--- assays/urls.py
+++ assays/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, url
 from assays.views import *
 
 urlpatterns = patterns('',
-    url(r'^assays/runs/$', RunList.as_view()),
-    url(r'^assays/new_read/$', manage_readouts),
+    url(r'^assays/runs/$', AssayRunList.as_view()),
+    url(r'^assays/new_read/$', add_readout),
     url(r'^assays/reads/$', AssayChipReadoutList.as_view()),
 )
