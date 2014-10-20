1e07212f108d719cc273659f700537fd5d3922d1
Luke-Malchow-Bergenthal <lmb140@pitt.edu>
2014-10-20 09:52:09 -0400

    Inline fix now checks if page valid; takes Change Tracking OR Reference Parameters

--- mps/static/js/inline_fix.js
+++ mps/static/js/inline_fix.js
@@ -1,23 +1,30 @@
 $(document).ready(function () {
     var index = 0;
 
-    for (i in $(".module")) {
-        if ($(".module")[i].outerHTML.indexOf("<h2>Change Tracking</h2>") > -1) {
-            index = i;
-            break;
+    if ($(".module")[1]) {
+
+        for (i in $(".module")) {
+            if ($(".module")[i].outerHTML.indexOf("<h2>Change Tracking</h2>") > -1 ||
+                $(".module")[i].outerHTML.indexOf("<h2>Reference Parameters</h2>") > -1) {
+                index = i;
+                break;
+            }
         }
+        $(".inline-group").insertBefore($(".module")[index]);
+
     }
-    $( ".inline-group" ).insertBefore( $( ".module" )[index]);
 
-# The following will move the inline in front of the Reference Parameters.
-# Note that the following will generate an error if "Reference Parameters" does
-# exist.  This is true in most cases and should be fixed at some point.
+// The following will move the inline in front of the Reference Parameters.
+// Note that the following will generate an error if "Reference Parameters" does
+// exist.  This is true in most cases and should be fixed at some point.
 
-    for (i in $(".module")) {
+/*    for (i in $(".module")) {
         if ($(".module")[i].outerHTML.indexOf("<h2>Reference Parameters</h2>") > -1) {
             index = i;
             break;
         }
     }
     $( ".inline-group" ).insertBefore( $( ".module" )[index]);
+*/
 });
+
