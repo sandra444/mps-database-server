91644e765092aa554d7e41bd23f6d78642fbafeb
ahgough <gough@pitt.edu>
2014-10-19 23:07:41 -0400

    Added comments in inline_fix.js.  Note that although this works now, it generates an error if there are no "Reference Parameters".  This should be fixed at some time.

--- mps/static/js/inline_fix.js
+++ mps/static/js/inline_fix.js
@@ -9,6 +9,10 @@ $(document).ready(function () {
     }
     $( ".inline-group" ).insertBefore( $( ".module" )[index]);
 
+# The following will move the inline in front of the Reference Parameters.
+# Note that the following will generate an error if "Reference Parameters" does
+# exist.  This is true in most cases and should be fixed at some point.
+
     for (i in $(".module")) {
         if ($(".module")[i].outerHTML.indexOf("<h2>Reference Parameters</h2>") > -1) {
             index = i;
