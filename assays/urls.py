ed7052e851ff2c6c6523e20997d435c86ceda5cc
Luke-Malchow-Bergenthal <lmb140@pitt.edu>
2014-10-28 16:08:07 -0400

    Very early details view

--- assays/urls.py
+++ assays/urls.py
@@ -6,5 +6,5 @@ urlpatterns = patterns('',
     url(r'^assays/new_read/$', add_readout),
     url(r'^assays/reads/$', AssayChipReadoutList.as_view()),
     url(r'^assays/assaychipreadout/add$', AssayChipReadoutAdd.as_view()),
-    url(r'^(?assays/assaychipreadout/detail/[-_\w]+)/$', AssayChipReadoutDetail.as_view(), name='readout-detail')
+    url(r'^assays/assaychipreadout/(?P<pk>[0-9]+)/$', AssayChipReadoutDetail.as_view(), name='readout-detail')
 )
