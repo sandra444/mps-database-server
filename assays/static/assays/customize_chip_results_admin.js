5e3d88624fb73d2bc98ddf28458121b8840792d5
Luke-Malchow-Bergenthal <lmb140@pitt.edu>
2014-10-30 10:49:33 -0400

    Quick fix for handling new entries to inline

--- assays/static/assays/customize_chip_results_admin.js
+++ assays/static/assays/customize_chip_results_admin.js
@@ -33,7 +33,7 @@ $(document).ready(function () {
     }
 
     var study = $('#id_assay_device_readout');
-    var newRow = $('.add-row');
+    var newRow = $('.add-row').children().children();
     var options = "";
 
     $.when(whittle('assay_run_id',study.val(),'AssayChipSetup','AssayChipReadout','chip_setup')).then(function(data) {
