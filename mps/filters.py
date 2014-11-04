421a353a7e0140556a5c1727e8994c5b8c0b15e4
Luke-Malchow-Bergenthal <lmb140@pitt.edu>
2014-11-04 17:24:55 -0500

    Early work for group privacy

--- mps/filters.py
+++ mps/filters.py
@@ -5,5 +5,8 @@ register = template.Library()
 
 @register.filter(name='has_group')
 def has_group(user, group_name):
+    if not group_name:
+        return True
+
     group = Group.objects.get(name=group_name)
     return True if group in user.groups.all() else False
