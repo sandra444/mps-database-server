195d4e64ae9529533da7fd645d8d1de8d341e7c7
Luke-Malchow-Bergenthal <lmb140@pitt.edu>
2014-11-06 11:04:11 -0500

    Prototype Readout add appears to be functional

--- assays/models.py
+++ assays/models.py
@@ -520,3 +520,6 @@ class AssayChipReadout(LockableModel):
 
     def __unicode__(self):
         return u'{0}'.format(self.chip_setup)
+
+    def get_absolute_url(self):
+        return "/assays/assaychipreadout/%i/" % self.id
