6ce6a14ff01a96139eb39342f1564604c5eedf24
Luke-Malchow-Bergenthal <lmb140@pitt.edu>
2014-11-06 10:12:10 -0500

    Early add 'button' working

--- mps/static/js/inline_add.js
+++ mps/static/js/inline_add.js
@@ -1,3 +1,12 @@
 $(document).ready(function () {
-    alert('inline_add loaded');
+    var inlines = $('.inline');
+    var next_id = inlines.length;
+    var title = $('.inline')[0].id.split('-')[0];
+    var add = $('.inline:first').html();
+
+    $('#add_button').click(function() {
+        var tag = '<div class="inline" id="' + title + '-' + next_id + '">';
+        $(tag).appendTo('#inlines').html(add);
+        next_id += 1;
+    });
 });
