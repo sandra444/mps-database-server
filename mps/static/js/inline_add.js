a794b7f54046415ee9688dc5a773ae94ff7e04ff
Luke-Malchow-Bergenthal <lmb140@pitt.edu>
2014-11-06 14:26:16 -0500

    Test adding in table and plot

--- mps/static/js/inline_add.js
+++ mps/static/js/inline_add.js
@@ -10,7 +10,7 @@ $(document).ready(function () {
     $('#add_button').click(function() {
         var tag = '<div class="inline" id="' + title + '-' + next_id + '">';
         // Use a regular expression to replace all the places where ID is needed
-        $(tag).appendTo('#inlines').html(add.replace(new RegExp('-0-', 'g'),'-'+next_id+'-'));
+        $(tag).appendTo($("div[name='inlines']")).html(add.replace(new RegExp('-0-', 'g'),'-'+next_id+'-'));
         next_id += 1;
         // Set the hidden TOTAL_FORMS to be incremented, otherwise won't bother reading other inline
         $("input[id*='TOTAL_FORMS']").val(""+next_id);
