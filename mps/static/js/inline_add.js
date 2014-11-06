04013266d058b68dbfe291de595045b830a76014
Luke-Malchow-Bergenthal <lmb140@pitt.edu>
2014-11-06 10:44:14 -0500

    Still need to fix uploading multiple assays

--- mps/static/js/inline_add.js
+++ mps/static/js/inline_add.js
@@ -1,4 +1,7 @@
 $(document).ready(function () {
+
+    //Note that this requires certain class name, add name, inlines name
+
     var inlines = $('.inline');
     var next_id = inlines.length;
     var title = $('.inline')[0].id.split('-')[0];
@@ -6,7 +9,8 @@ $(document).ready(function () {
 
     $('#add_button').click(function() {
         var tag = '<div class="inline" id="' + title + '-' + next_id + '">';
-        $(tag).appendTo('#inlines').html(add);
+        // Use a regular expression to replace all the places where ID is needed
+        $(tag).appendTo('#inlines').html(add.replace(new RegExp('-0-', 'g'),'-'+next_id+'-'));
         next_id += 1;
     });
 });
