3da981b63221b5a3776efadb5ffac7a7a22c8ac5
Luke-Malchow-Bergenthal <lmb140@pitt.edu>
2014-11-07 18:15:45 -0500

    Simple search now functional

--- compounds/views.py
+++ compounds/views.py
@@ -11,6 +11,17 @@ class CompoundList(ListView):
     template_name = 'compounds/compounds.html'
     paginate_by = 50
 
+    def get_queryset(self):
+        try:
+            name = self.request.GET['name']
+        except:
+            name = ''
+        if (name != ''):
+            object_list = self.model.objects.filter(name__icontains = name)
+        else:
+            object_list = self.model.objects.all()
+        return object_list
+
 class CompoundDetails(DetailView):
     model = Compound
     template_name = 'compounds/compound.html'
