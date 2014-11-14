efd5f0157129ce951e264e96f949771f67316c35
Luke-Malchow-Bergenthal <lmb140@pitt.edu>
2014-11-14 15:13:09 -0500

    Early work for studies

--- assays/urls.py
+++ assays/urls.py
@@ -5,4 +5,7 @@ urlpatterns = patterns('',
     url(r'^assays/assaychipreadout/$', AssayChipReadoutList.as_view()),
     url(r'^assays/assaychipreadout/add$', AssayChipReadoutAdd.as_view()),
     url(r'^assays/assaychipreadout/(?P<pk>[0-9]+)/$', AssayChipReadoutDetail.as_view(), name='readout-detail'),
+    url(r'^assays/organchipstudy/$', AssayRunList.as_view()),
+    url(r'^assays/organchipstudy/add$', AssayRunAdd.as_view()),
+    url(r'^assays/organchipstudy/(?P<pk>[0-9]+)/$', AssayRunDetail.as_view(), name='study-detail'),
 )
--- assays/views.py
+++ assays/views.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 
 from django.views.generic import ListView, CreateView, DetailView
-from assays.models import AssayChipReadout
+from assays.models import *
 
 class AssayChipReadoutList(ListView):
     model = AssayChipReadout
@@ -10,9 +10,10 @@ from assays.forms import AssayChipReadoutForm
 
 from django.forms.models import inlineformset_factory
 from django.views.generic.edit import CreateView
-from assays.models import AssayChipReadoutAssay
-from assays.admin import AssayChipReadoutInlineFormset, parseChipCSV
+from assays.models import *
+from assays.admin import *
 from django.shortcuts import redirect
+# May be useful later
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.utils.decorators import method_decorator
 
@@ -70,3 +71,34 @@ class AssayChipReadoutDetail(DetailView):
             return self.render_to_response({'now':timezone.now()}) # Just date for now
         context = super(AssayChipReadoutDetail, self).get_context_data(**kwargs)
         return self.render_to_response(context)
+
+# Class-based views for studies
+
+class AssayRunList(ListView):
+    model = AssayRun
+
+class AssayRunAdd(CreateView):
+    template_name = 'assays/assayrun_add.html'
+    form_class = AssayRunForm
+
+    # Test form validity
+    def form_valid(self, form):
+        context = self.get_context_data()
+        formset = context['formset']
+        # get user via self.request.user
+        if formset.is_valid():
+            self.object = form.save()
+            self.object.modified_by = self.object.created_by = self.request.user
+            # Save Chip Readout
+            self.object.save()
+            formset.instance = self.object
+            formset.save()
+            if formset.__dict__['files']:
+                file = formset.__dict__['files']['file']
+                parseRunCSV(self.object,file)
+            return redirect(self.object.get_absolute_url())  # assuming your model has ``get_absolute_url`` defined.
+        else:
+            return self.render_to_response(self.get_context_data(form=form))
+
+class AssayRunDetail(DetailView):
+    model = AssayRun
