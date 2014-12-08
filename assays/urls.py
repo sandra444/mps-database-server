fac6e73013e672d89c600b6b62315fa167c68016
Luke-Malchow-Bergenthal <lmb140@pitt.edu>
2014-12-08 13:00:41 -0500

    Change URL regex

--- assays/urls.py
+++ assays/urls.py
@@ -7,16 +7,16 @@ urlpatterns = patterns('',
     url(r'^assays/user_index/$', UserIndex.as_view(), name='user_index'),
     # User can view all group studies
     url(r'^assays/group_index/$', GroupIndex.as_view(), name='group_index'),
-    # # The main page for a study
+    # The main page for a study
     url(r'^assays/(?P<study_id>[0-9]+)/$', StudyIndex.as_view(), name='study_index'),
     # # Change pages for respective models
-    # url(r'^assays/(?P<study_id>d+)/assaychipsetup/?P<setup_id>d+/$', XXX),
-    # url(r'^assays/(?P<study_id>d+)/assaychipreadout/?P<readout_id>d+/$', XXX),
-    # url(r'^assays/(?P<study_id>d+)/assaytestresult/?P<result_id>d+/$', XXX),
+    # url(r'^assays/(?P<study_id>[0-9]+)/assaychipsetup/?P<setup_id>[0-9]+/$', XXX),
+    # url(r'^assays/(?P<study_id>[0-9]+)/assaychipreadout/?P<readout_id>[0-9]+/$', XXX),
+    # url(r'^assays/(?P<study_id>[0-9]+)/assaytestresult/?P<result_id>[0-9]+/$', XXX),
     # # Add pages for respective models
-    # url(r'^assays/(?P<study_id>d+)/assaychipsetup/add/$', XXX),
-    # url(r'^assays/(?P<study_id>d+)/assaychipreadout/add/$', XXX),
-    # url(r'^assays/(?P<study_id>d+)/assaytestresult/add/$', XXX),
+    # url(r'^assays/(?P<study_id>[0-9]+)/assaychipsetup/add/$', XXX),
+    # url(r'^assays/(?P<study_id>[0-9]+)/assaychipreadout/add/$', XXX),
+    # url(r'^assays/(?P<study_id>[0-9]+)/assaytestresult/add/$', XXX),
 
     # Original URLS: Add views to be replaced by new interface
     # TODO limit list and detail views to unrestricted models
