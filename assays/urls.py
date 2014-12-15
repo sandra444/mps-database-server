a94b78fdd729d5636c69710ace4bb8574a4772e5
Luke-Malchow-Bergenthal <lmb140@pitt.edu>
2014-12-15 12:19:42 -0500

    Change URL names

--- assays/urls.py
+++ assays/urls.py
@@ -21,18 +21,18 @@ urlpatterns = patterns('',
     # Original URLS: Add views to be replaced by new interface
     # TODO limit list and detail views to unrestricted models
     url(r'^assays/organchipstudy/$', AssayRunList.as_view()),
-    url(r'^assays/organchipstudy/add$', AssayRunAdd.as_view()),
-    url(r'^assays/organchipstudy/(?P<pk>[0-9]+)/$', AssayRunDetail.as_view(), name='study-detail'),
+    url(r'^assays/organchipstudy/add$', AssayRunAdd.as_view(), name='study_add'),
+    url(r'^assays/organchipstudy/(?P<pk>[0-9]+)/$', AssayRunDetail.as_view(), name='study_detail'),
 
     url(r'^assays/assaychipsetup/$', AssayChipSetupList.as_view()),
     #url(r'^assays/assaychipsetup/add$', AssayChipSetupAdd.as_view()),
-    url(r'^assays/assaychipsetup/(?P<pk>[0-9]+)/$', AssayChipSetupDetail.as_view(), name='setup-detail'),
+    url(r'^assays/assaychipsetup/(?P<pk>[0-9]+)/$', AssayChipSetupDetail.as_view(), name='setup_detail'),
 
     url(r'^assays/assaychipreadout/$', AssayChipReadoutList.as_view()),
     #url(r'^assays/assaychipreadout/add$', AssayChipReadoutAdd.as_view()),
-    url(r'^assays/assaychipreadout/(?P<pk>[0-9]+)/$', AssayChipReadoutDetail.as_view(), name='readout-detail'),
+    url(r'^assays/assaychipreadout/(?P<pk>[0-9]+)/$', AssayChipReadoutDetail.as_view(), name='readout_detail'),
 
     url(r'^assays/assaytestresult/$', AssayTestResultList.as_view()),
     #url(r'^assays/assaytestresult/add$', AssayTestResultAdd.as_view()),
-    url(r'^assays/assaytestresult/(?P<pk>[0-9]+)/$', AssayTestResultDetail.as_view(), name='result-detail'),
+    url(r'^assays/assaytestresult/(?P<pk>[0-9]+)/$', AssayTestResultDetail.as_view(), name='result_detail'),
 )
