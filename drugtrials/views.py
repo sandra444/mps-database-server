4b6d58503764d48a93e5c2ef8c8185523b469e76
Luke-Malchow-Bergenthal <lmb140@pitt.edu>
2015-01-13 11:09:19 -0500

    Drugtrials should now load in milliseconds

--- drugtrials/views.py
+++ drugtrials/views.py
@@ -2,5 +2,9 @@ from .models import FindingResult
 from django.views.generic import ListView
 
 class DrugTrialList(ListView):
-    model = FindingResult
+    #model = FindingResult
     template_name = 'drugtrials/drugtrial_list.html'
+
+    def get_queryset(self):
+        queryset = FindingResult.objects.prefetch_related('drug_trial','finding_name', 'value_units').select_related('drug_trial__compound', 'drug_trial__species', 'finding_name__organ', 'finding_name__finding_type').all()
+        return queryset
