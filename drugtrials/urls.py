75cabac4b94aa0793b680b1d0393e4e13861ef12
Luke-Malchow-Bergenthal <lmb140@pitt.edu>
2015-01-23 11:29:04 -0500

    Initial drugtrial display view

--- drugtrials/urls.py
+++ drugtrials/urls.py
@@ -5,4 +5,5 @@ urlpatterns = patterns('',
     # Proposed URLS:
     # User can view their studies
     url(r'^drugtrials/$', DrugTrialList.as_view(), name='drugtrial_list'),
+    url(r'^drugtrials/(?P<pk>[0-9]+)/$', drug_trial_detail, name='drugtrial_detail'),
 )
--- drugtrials/views.py
+++ drugtrials/views.py
@@ -1,5 +1,7 @@
-from .models import FindingResult
+from .models import FindingResult,  DrugTrial
 from django.views.generic import ListView
+from django.shortcuts import render_to_response, get_object_or_404
+from django.template import RequestContext
 
 class DrugTrialList(ListView):
     #model = FindingResult
@@ -8,3 +10,16 @@ class DrugTrialList(ListView):
     def get_queryset(self):
         queryset = FindingResult.objects.prefetch_related('drug_trial','finding_name', 'value_units').select_related('drug_trial__compound', 'drug_trial__species', 'finding_name__organ', 'finding_name__finding_type').all()
         return queryset
+
+def drug_trial_detail(request, *args, **kwargs):
+    c = RequestContext(request)
+
+    trial = get_object_or_404(DrugTrial, pk=kwargs.get('pk'))
+    results = FindingResult.objects.filter(drug_trial=trial).prefetch_related('drug_trial','finding_name', 'value_units').select_related('drug_trial__compound', 'drug_trial__species', 'finding_name__organ', 'finding_name__finding_type')
+
+    c.update({
+        'trial': trial,
+        'results': results,
+    })
+
+    return render_to_response('drugtrials/drugtrial_detail.html', c)
