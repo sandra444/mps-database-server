6d76b1b12c6d40e4d3b2456c221e159f5e44a5c1
Luke-Malchow-Bergenthal <lmb140@pitt.edu>
2015-02-05 16:25:47 -0500

    Initial work for Readout Updates

--- assays/urls.py
+++ assays/urls.py
@@ -11,7 +11,7 @@ urlpatterns = patterns('',
     url(r'^assays/(?P<study_id>[0-9]+)/$', StudyIndex.as_view(), name='study_index'),
     # # Change pages for respective models
     # url(r'^assays/(?P<study_id>[0-9]+)/assaychipsetup/(?P<pk>[0-9]+)/$', XXX),
-    # url(r'^assays/(?P<study_id>[0-9]+)/assaychipreadout/(?P<pk>[0-9]+)/$', XXX),
+    url(r'^assays/(?P<study_id>[0-9]+)/assaychipreadout/(?P<pk>[0-9]+)/$', AssayChipReadoutUpdate.as_view(), name='readout_update'),
     url(r'^assays/(?P<study_id>[0-9]+)/assaytestresult/(?P<pk>[0-9]+)/$', AssayTestResultUpdate.as_view(), name='result_update'),
     # # Add pages for respective models
     url(r'^assays/(?P<study_id>[0-9]+)/assaychipsetup/add/$', AssayChipSetupAdd.as_view(), name='setup_add'),
