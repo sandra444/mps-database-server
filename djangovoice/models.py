6b3369d2b0d127b3d0e03a40d7d82d9394cff554
Luke-Malchow-Bergenthal <lmb140@pitt.edu>
2015-02-11 15:41:43 -0500

    Djangovoice files (will be modified soon)

---
+++ djangovoice/models.py
@@ -0,0 +1,109 @@
+from django.db import models
+from django.utils.translation import pgettext
+from django.utils.translation import ugettext_lazy as _
+from djangovoice.compat import User
+from qhonuskan_votes.models import VotesField
+from qhonuskan_votes.models import ObjectsWithScoresManager
+
+STATUS_CHOICES = (
+    ('open', pgettext('status', "Open")),
+    ('closed', pgettext('status', "Closed")),
+)
+
+
+class Status(models.Model):
+    title = models.CharField(max_length=500)
+    slug = models.SlugField(max_length=500)
+    default = models.BooleanField(
+        blank=True,
+        help_text=_("New feedback will have this status"))
+    status = models.CharField(
+        max_length=10, choices=STATUS_CHOICES, default=STATUS_CHOICES[0][0])
+
+    def save(self, **kwargs):
+        if self.default:
+            try:
+                default_project = Status.objects.get(default=True)
+                default_project.default = False
+                default_project.save()
+
+            except Status.DoesNotExist:
+                pass
+
+        super(Status, self).save(**kwargs)
+
+    def __unicode__(self):
+        return unicode(self.title)
+
+    class Meta:
+        verbose_name = _("status")
+        verbose_name_plural = _("statuses")
+
+
+class Type(models.Model):
+    title = models.CharField(max_length=500)
+    slug = models.SlugField(max_length=500)
+
+    def __unicode__(self):
+        return self.title
+
+
+class Feedback(models.Model):
+    title = models.CharField(max_length=500, verbose_name=_("Title"))
+    description = models.TextField(
+        blank=True, verbose_name=_("Description"),
+        help_text=_(
+            "This will be viewable by other people - do not include any "
+            "private details such as passwords or phone numbers here."))
+    type = models.ForeignKey(Type, verbose_name=_("Type"))
+    anonymous = models.BooleanField(
+        blank=True, verbose_name=_("Anonymous"),
+        help_text=_("Do not show who sent this"))
+    private = models.BooleanField(
+        verbose_name=_("Private"), blank=True,
+        help_text=_(
+            "Hide from public pages. Only site administrators will be able to "
+            "view and respond to this"))
+    user = models.ForeignKey(
+        User, blank=True, null=True, verbose_name=_("User"))
+    email = models.EmailField(
+        blank=True, null=True, verbose_name=_('E-mail'),
+        help_text=_(
+            "You must provide your e-mail so we can answer to you. "
+            "Alternatively you can bookmark next page and check out for an "
+            "answer later."))
+    slug = models.SlugField(max_length=10, blank=True, null=True)
+    created = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    status = models.ForeignKey(Status, verbose_name=_('Status'))
+    duplicate = models.ForeignKey(
+        'self', null=True, blank=True, verbose_name=_("Duplicate"))
+    votes = VotesField()
+    objects = ObjectsWithScoresManager()
+
+    def save(self, **kwargs):
+        try:
+            self.status
+
+        except Status.DoesNotExist:
+            try:
+                default = Status.objects.get(default=True)
+
+            except Status.DoesNotExist:
+                default = Status.objects.all()[0]
+
+            self.status = default
+
+        super(Feedback, self).save(**kwargs)
+
+    @models.permalink
+    def get_absolute_url(self):
+        return 'djangovoice_item', [self.id]
+
+    def __unicode__(self):
+        return unicode(self.title)
+
+    class Meta:
+        verbose_name = _("feedback")
+        verbose_name_plural = _("feedback")
+        ordering = ('-created',)
+        get_latest_by = 'created'
---
+++ djangovoice/tests.py
@@ -0,0 +1,56 @@
+from django.core.urlresolvers import reverse
+from django.test import Client, TestCase
+
+from djangovoice.models import *
+
+
+class ViewTestCase(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+
+    def test_feedback_list_page(self):
+        # user is not logged in, but it can see feedback list:
+        response = self.client.get(reverse('djangovoice_home'))
+        self.assertEqual(response.status_code, 200)
+
+
+class StatusTestCase(TestCase):
+    def setUp(self):
+        self.in_progress = Status.objects.create(
+            title='In progress', slug='in_progress', default=False)
+        self.need_to_test = Status.objects.create(
+            title='Need to test', slug='need_to_test', default=True)
+
+    def testSpeaking(self):
+        self.assertEqual(self.in_progress.status, 'open')
+        self.assertEqual(self.need_to_test.default, True)
+
+
+class TypeTestCase(TestCase):
+    def setUp(self):
+        self.bug = Type.objects.create(title='Bug', slug='bug')
+        self.betterment = Type.objects.create(title='Betterment',
+                                              slug='betterment')
+
+    def testSpeaking(self):
+        self.assertEqual(self.bug.slug, 'bug')
+        self.assertEqual(self.betterment.title, 'Betterment')
+
+
+class FeedbackTestCase(TestCase):
+    def setUp(self):
+        feedback_type = Type.objects.create(title='Bug', slug='bug')
+        feedback_user = User.objects.create_user(
+            username='djangovoice', email='django@voice.com')
+        self.login_form_does_not_work = Feedback.objects.create(
+            type=feedback_type,
+            title='Login form does not work.',
+            description='What a fucking test...',
+            anonymous=False,
+            private=True,
+            user=feedback_user)
+
+    def testSpeaking(self):
+        default_status = Status.objects.get(default=True)
+        self.assertEqual(self.login_form_does_not_work.status, default_status)
