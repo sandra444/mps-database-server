0922eda09486565ffbfcf39a396e7a6b0526863c
Luke-Malchow-Bergenthal <lmb140@pitt.edu>
2015-03-25 15:15:00 -0400

    Alert users if they are overwriting data (currently just default JS alert)

--- mps/static/js/enter_override.js
+++ mps/static/js/enter_override.js
@@ -1,6 +1,6 @@
 // Adding this script will override enter such that it will display a modal confirmation
 $(document).ready(function () {
-    $("#content").append('<div hidden id="dialog-confirm" title="Submit this form?"><p><span class="ui-icon ui-icon-alert" style="float:left; margin:0 7px 20px 0;"></span>Are you sure you want to submit the form?</p></div>');
+    $("#content").append('<div hidden id="dialog-confirm" title="Submit this form?"><p><span class="glyphicon glyphicon-exclamation-sign text-danger" aria-hidden="true" style="float:left; margin:0 7px 20px 0;"></span>Are you sure you want to submit the form?</p></div>');
 
     var dialogConfirm = $("#dialog-confirm");
 
