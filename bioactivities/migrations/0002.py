a61e8a4be0753fc980d29e39dca8f44d4112719d
Luke-Malchow-Bergenthal <lmb140@pitt.edu>
2015-05-21 10:09:27 -0400

    Newest migrations from Prody

---
+++ bioactivities/migrations/0002.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('bioactivities', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='PubChemAssay',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('created_on', models.DateTimeField(auto_now_add=True, null=True)),
+                ('modified_on', models.DateTimeField(auto_now=True, null=True)),
+                ('signed_off_date', models.DateTimeField(null=True, blank=True)),
+                ('locked', models.BooleanField(default=False, help_text=b'Check the box and save to lock the entry. Uncheck and save to enable editing.')),
+                ('source', models.TextField(default=b'', null=True, blank=True)),
+                ('source_id', models.TextField(default=b'', null=True, blank=True)),
+                ('target_type', models.TextField(default=b'', null=True, blank=True)),
+                ('organism', models.TextField(default=b'', null=True, blank=True)),
+                ('aid', models.TextField(verbose_name=b'Assay ID')),
+                ('name', models.TextField(default=b'', null=True, verbose_name=b'Assay Name', blank=True)),
+                ('description', models.TextField(null=True, blank=True)),
+                ('created_by', models.ForeignKey(related_name='pubchemassay_created-by', blank=True, to=settings.AUTH_USER_MODEL, null=True)),
+                ('modified_by', models.ForeignKey(related_name='pubchemassay_modified-by', blank=True, to=settings.AUTH_USER_MODEL, null=True)),
+                ('signed_off_by', models.ForeignKey(related_name='pubchemassay_signed_off_by', blank=True, to=settings.AUTH_USER_MODEL, null=True)),
+            ],
+            options={
+                'abstract': False,
+            },
+            bases=(models.Model,),
+        ),
+        migrations.RemoveField(
+            model_name='pubchembioactivity',
+            name='assay_id',
+        ),
+        migrations.RemoveField(
+            model_name='pubchembioactivity',
+            name='assay_name',
+        ),
+        migrations.RemoveField(
+            model_name='pubchembioactivity',
+            name='source',
+        ),
+        migrations.AddField(
+            model_name='pubchembioactivity',
+            name='assay',
+            field=models.ForeignKey(blank=True, to='bioactivities.PubChemAssay', null=True),
+            preserve_default=True,
+        ),
+    ]
---
+++ compounds/migrations/0002.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('compounds', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='compound',
+            name='tags',
+            field=models.TextField(help_text=b'Tags for the compound (EPA, NCATS, etc.)', null=True, blank=True),
+            preserve_default=True,
+        ),
+    ]
