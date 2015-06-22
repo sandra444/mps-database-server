2906b81e17817799c4bb8b1a987a5106fd11b0c2
Luke-Malchow-Bergenthal <lmb140@pitt.edu>
2015-06-22 09:43:58 -0400

    Resolve bug when selecting cancel for update submissions

--- mps/static/js/enter_override.js
+++ mps/static/js/enter_override.js
@@ -2,10 +2,14 @@
 // This script will additionally help prevent the multiple submission issue
 $(document).ready(function () {
     // On submit, disable all submit buttons
-    $('form').submit(function(){
-        $(':submit').attr('disabled', 'disabled');
-        return true;
-    });
+    // This only really matters on add pages and causes a bug on update pages when 'cancel' is selected
+    // if 'Edit ' in h1 is a crude test to determine whether update or not
+    if (!($('h1').first().text().indexOf('Edit ') > -1)) {
+        $('form').submit(function () {
+            $(':submit').attr('disabled', 'disabled');
+            return true;
+        });
+    }
 
     // Add the dialog box
     $("#content").append('<div hidden id="dialog-confirm" title="Submit this form?"><p><span class="glyphicon glyphicon-exclamation-sign text-danger" aria-hidden="true" style="float:left; margin:0 7px 20px 0;"></span>Are you sure you want to submit the form?</p></div>');
