4218af11f13c593a830eb38c1bb9e73cf931fadb
Luke-Malchow-Bergenthal <lmb140@pitt.edu>
2015-08-25 17:07:28 -0400

    Line breaks should now be properly maintained for the compound report

--- compounds/static/compounds/compound_report.js
+++ compounds/static/compounds/compound_report.js
@@ -112,11 +112,11 @@ $(document).ready(function () {
             row += "<td><a href='/compounds/"+values['id']+"'>" + compound + "</a></td>";
             row += "<td>" + values['Dose (xCmax)'] + "</td>";
             row += "<td>" + values['cLogP']  + "</td>";
-            row += "<td>" + values['Pre-clinical Findings'] + "</td>";
-            row += "<td>" + values['Clinical Findings'] + "</td>";
+            row += "<td>" + values['Pre-clinical Findings'].replace(/\r\n/g,'<br>') + "</td>";
+            row += "<td>" + values['Clinical Findings'].replace(/\r\n/g,'<br>') + "</td>";
 
             // Recently added
-            row += "<td>" + values['PK/Metabolism'] + "</td>";
+            row += "<td>" + values['PK/Metabolism'].replace(/\r\n/g,'<br>') + "</td>";
 
             row += "<td>";
 
