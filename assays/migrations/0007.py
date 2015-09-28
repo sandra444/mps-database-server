3325cfa7e0653ce6e65164da2313ff360bff7880
Luke-Malchow-Bergenthal <lmb140@pitt.edu>
2015-09-28 15:17:42 -0400

    Newest migration from prody

---
+++ assays/migrations/0007.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('assays', '0006'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='assaychiprawdata',
+            name='quality',
+            field=models.CharField(default=b'', max_length=20),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='assaychiptestresult',
+            name='summary',
+            field=models.TextField(default=b''),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='assayplatetestresult',
+            name='summary',
+            field=models.TextField(default=b''),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='assayrun',
+            name='image',
+            field=models.ImageField(null=True, upload_to=b'studies', blank=True),
+            preserve_default=True,
+        ),
+    ]
