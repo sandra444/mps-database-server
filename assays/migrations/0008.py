74b5cd457a8601150d467eb9285636bd140f6706
Luke-Malchow-Bergenthal <lmb140@pitt.edu>
2015-11-09 12:53:56 -0500

    Newest migrations from Prody

---
+++ assays/migrations/0008.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import assays.models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('assays', '0007'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='assaylayout',
+            name='standard',
+            field=models.BooleanField(default=False),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='assayreadout',
+            name='quality',
+            field=models.CharField(default=b'', max_length=10),
+            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='assaychipreadout',
+            name='file',
+            field=models.FileField(help_text=b'Green = Data from database; Red = Line that will not be read; Gray = Reading with null value ***Uploading overwrites old data***', upload_to=assays.models.chip_readout_file_location, null=True, verbose_name=b'Data File', blank=True),
+            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='assaychiptestresult',
+            name='summary',
+            field=models.TextField(default=b'', blank=True),
+            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='assayplatereadout',
+            name='file',
+            field=models.FileField(upload_to=assays.models.plate_readout_file_location, null=True, verbose_name=b'Data File', blank=True),
+            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='assayplatetestresult',
+            name='summary',
+            field=models.TextField(default=b'', blank=True),
+            preserve_default=True,
+        ),
+        migrations.AlterUniqueTogether(
+            name='assayplatereadoutassay',
+            unique_together=set([('readout_id', 'assay_id', 'feature')]),
+        ),
+    ]
---
+++ cellsamples/migrations/0003.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('cellsamples', '0002'),
+    ]
+
+    operations = [
+        migrations.RemoveField(
+            model_name='cellsample',
+            name='viable_count_unit',
+        ),
+        migrations.AddField(
+            model_name='cellsample',
+            name='cell_subtype',
+            field=models.ForeignKey(default=1, to='cellsamples.CellSubtype'),
+            preserve_default=False,
+        ),
+        migrations.AddField(
+            model_name='cellsample',
+            name='flagged',
+            field=models.BooleanField(default=False, help_text=b'Check box to flag for review'),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='cellsample',
+            name='reason_for_flag',
+            field=models.CharField(help_text=b'Reason for why this entry was flagged', max_length=300, null=True, blank=True),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='cellsubtype',
+            name='cell_type',
+            field=models.ForeignKey(blank=True, to='cellsamples.CellType', null=True),
+            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='celltype',
+            name='cell_subtype',
+            field=models.ForeignKey(blank=True, to='cellsamples.CellSubtype', null=True),
+            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='celltype',
+            name='species',
+            field=models.CharField(default=b'Human', max_length=10, null=True, blank=True, choices=[(b'Human', b'Human'), (b'Rat', b'Rat'), (b'Mouse', b'Mouse')]),
+            preserve_default=True,
+        ),
+    ]
