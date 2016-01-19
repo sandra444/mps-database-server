191b0e01618eb6467f2a8c9437815dc08e4c47ed
Luke-Malchow-Bergenthal <lmb140@pitt.edu>
2016-01-19 17:12:00 -0500

    Newest migration from prody

---
+++ assays/migrations/0015.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('assays', '0014'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='physicalunits',
+            name='unit_type',
+            field=models.ForeignKey(default=8, to='assays.UnitType'),
+            preserve_default=False,
+        ),
+    ]
