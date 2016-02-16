65f2c1411d1e13a37deb577b47608ca18d00be00
Luke-Malchow-Bergenthal <lmb140@pitt.edu>
2016-02-16 13:42:56 -0500

    Add five more colors to the bar graph pattern (not optimal selections, lighter oft too similar to others)

--- drugtrials/static/drugtrials/compare_adverse_events.js
+++ drugtrials/static/drugtrials/compare_adverse_events.js
@@ -69,7 +69,13 @@ $(document).ready(function () {
                     '#1f77b4', '#aec7e8', '#ff7f0e', '#ffbb78', '#2ca02c',
                     '#98df8a', '#d62728', '#ff9896', '#9467bd', '#c5b0d5',
                     '#8c564b', '#c49c94', '#e377c2', '#f7b6d2', '#7f7f7f',
-                    '#c7c7c7', '#bcbd22', '#dbdb8d', '#17becf', '#9edae5'
+                    '#c7c7c7', '#bcbd22', '#dbdb8d', '#17becf', '#9edae5',
+                    '#18F285', '#A8EDCB',
+                    '#E6F02E', '#E5E8A5',
+                    '#AAF514', '#CDF283',
+                    '#52400B', '#635B42',
+                    '#CCCCCC', '#E3E3E3'
+
                 ]
             },
             // Consider way to deal with overbearing tooltips
