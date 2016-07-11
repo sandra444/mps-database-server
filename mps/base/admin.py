51dfdba1fa32b2c4c39b5f1794a5dae71662be46
Luke-Malchow-Bergenthal <lmb140@pitt.edu>
2016-07-11 17:09:08 -0400

    Clean up base App

--- mps/base/admin.py
+++ mps/base/admin.py
@@ -1,38 +1,27 @@
 # coding=utf-8
 
-"""
-
-TrackableAdmin should be the default class inheirited by other Admin modules
+"""TrackableAdmin should be the default class inherited by other Admin modules
 
 This class implements timestamps on each item generated including
 who created the item and who last edited the item
-
-
 """
 
 from import_export.admin import ImportExportModelAdmin
 
 
 class TrackableAdmin(ImportExportModelAdmin):
-    """
-
-    The class that all other "normal" admin classes subclass by default.
+    """The class that all other "normal" admin classes subclass by default.
 
     "Normal" being defined as a class that ought to subclass admin.ModelAdmin
     However, our TrackableAdmin will provide additional user access info.
-
     """
 
     def save_model(self, request, obj, form, change):
-
-        """
-
-        Given a model instance save it to the database.
+        """Given a model instance save it to the database.
 
         Update modified_by and created_by when the model is created
         otherwise update the modified_by field since the object was saved
         and modified
-
         """
 
         if change:
@@ -52,12 +41,9 @@ class TrackableAdmin(ImportExportModelAdmin):
 
 
 class LockableAdmin(TrackableAdmin):
-    """
-
-    The non-trackable Locking admin class
+    """The Locking admin class
 
     Use TrackableLockableAdmin to also enable user tracking features
-
     """
 
     actions = ['enable_editing_entries', 'disable_editing_entries']
