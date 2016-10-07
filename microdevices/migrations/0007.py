87584f283f0eabde581418b536d11b256038c95f
Luke-Malchow-Bergenthal <lmb140@pitt.edu>
2016-10-07 17:04:59 -0400

    Newest migrations from prody

---
+++ microdevices/migrations/0007.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('microdevices', '0006'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='organmodel',
+            name='tctc',
+            field=models.BooleanField(default=False, help_text=b'Whether this compound is part of the TCTC project'),
+        ),
+    ]
