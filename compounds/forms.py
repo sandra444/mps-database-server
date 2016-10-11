75df403c155bd641af1a4c907be29c122dd70aa4
Luke-Malchow-Bergenthal <lmb140@pitt.edu>
2016-10-11 14:57:39 -0400

    Revise compound form to have a larger field for drug class; move drug_class position; shrink INCHIkey field

--- compounds/forms.py
+++ compounds/forms.py
@@ -11,9 +11,10 @@ class CompoundForm(forms.ModelForm):
             'name': forms.Textarea(attrs={'rows': 1}),
             'tags': forms.Textarea(attrs={'rows': 1}),
             'smiles': forms.Textarea(attrs={'size': 50, 'rows': 3}),
-            'inchikey': forms.Textarea(attrs={'size': 50, 'rows': 3}),
+            'inchikey': forms.Textarea(attrs={'size': 50, 'rows': 1}),
             'molecular_formula': forms.Textarea(attrs={'size': 50, 'rows': 3}),
             'ro5_violations': forms.TextInput(attrs={'size': 2}),
+            'drug_class': forms.Textarea(attrs={'rows': 1}),
 
             'clearance': forms.Textarea(attrs={'size': 50, 'rows': 3}),
             'absorption': forms.Textarea(attrs={'size': 50, 'rows': 3}),
