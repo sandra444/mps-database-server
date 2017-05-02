a448c0f7c9f17cfdf051913c4025ef9b579318cc
Luke-Malchow-Bergenthal <lmb140@pitt.edu>
2017-05-02 17:10:33 -0400

    Newest migrations from Prody

---
+++ microdevices/migrations/0009.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import django.core.validators
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('microdevices', '0008'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='microdevice',
+            name='number_of_columns',
+            field=models.IntegerField(blank=True, null=True, validators=[django.core.validators.MaxValueValidator(100)]),
+        ),
+        migrations.AlterField(
+            model_name='microdevice',
+            name='number_of_rows',
+            field=models.IntegerField(blank=True, null=True, validators=[django.core.validators.MaxValueValidator(100)]),
+        ),
+    ]
