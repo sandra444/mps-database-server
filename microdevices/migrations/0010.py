711f533ac04d30a2abe252c57957eb36220d97b6
Luke-Malchow-Bergenthal <lmb140@pitt.edu>
2017-07-28 17:32:49 -0400

    New migrations from production

---
+++ microdevices/migrations/0010.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('microdevices', '0009'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='organmodel',
+            name='center',
+            field=models.ForeignKey(default=1, to='microdevices.MicrophysiologyCenter'),
+            preserve_default=False,
+        ),
+    ]
