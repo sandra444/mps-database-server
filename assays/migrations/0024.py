d15587b530f2e627063f4fc5e77a184fdc9bec3b
Luke-Malchow-Bergenthal <lmb140@pitt.edu>
2017-08-22 13:53:54 -0400

    Newest migration from production

---
+++ assays/migrations/0024.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('assays', '0023'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='assaychipresult',
+            name='assay_name',
+            field=models.ForeignKey(verbose_name=b'Assay', to='assays.AssayInstance'),
+        ),
+    ]
