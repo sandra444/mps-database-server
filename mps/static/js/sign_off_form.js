88147dee1172f95f4c3a2ba975b98bea8072d999
Luke-Malchow-Bergenthal <lmb140@pitt.edu>
2017-09-13 13:32:05 -0400

    Change sign off/verification confirm alerts

--- mps/static/js/sign_off_form.js
+++ mps/static/js/sign_off_form.js
@@ -16,11 +16,9 @@ $(document).ready(function() {
 
     if (study_submit) {
         dialog_title = 'Study Sign Off';
-        sign_off_confirm_warning.html('Signing off will prevent anyone from editing this Study and the data associated with it.<br><br>Are you sure you want to change the sign off status?');
     }
     else {
         dialog_title = 'Entry Validation';
-        sign_off_confirm_warning.html('If this is checked, the entry will be marked as Validated.<br><br>Are you sure you want to change the validated status?');
     }
 
     function set_labels() {
@@ -28,25 +26,43 @@ $(document).ready(function() {
             mark_reviewed_check.show();
             if (study_submit) {
                 mark_reviewed_label.text('Click Here to Remove Sign Off');
+                sign_off_confirm_warning.html(
+                    'Removing the Sign Off will again allow users to edit this Study and the data associated with it.' +
+                    '<br><br>Are you sure you want to change the sign off status?'
+                );
+
             }
             else {
                 mark_reviewed_label.text('Click Here to Remove Validated Mark');
+                sign_off_confirm_warning.html(
+                    'If you continue, this entry will no longer be marked as Validated.' +
+                    '<br><br>Are you sure you want to change the validated status?'
+                );
             }
         }
         else {
             mark_reviewed_check.hide();
             if (study_submit) {
                 mark_reviewed_label.text('Click Here to Sign Off on this Study');
+                sign_off_confirm_warning.html(
+                    'Signing Off will prevent anyone from editing this Study and the data associated with it.' +
+                    '<br><br>Are you sure you want to change the sign off status?'
+                );
+
             }
             else {
                 mark_reviewed_label.text('Click Here to Mark this Entry as Validated');
+                sign_off_confirm_warning.html(
+                    'If this is checked, the entry will be marked as Validated.' +
+                    '<br><br>Are you sure you want to change the validated status?'
+                );
             }
         }
     }
 
     sign_off_confirm.dialog({
         title: dialog_title,
-        height:325,
+        height:330,
         modal: true,
         closeOnEscape: true,
         autoOpen: false,
