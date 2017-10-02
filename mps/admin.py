a83c62b42382b6f8d79de5a843436cfcc42296bb
Luke-Malchow-Bergenthal <lmb140@pitt.edu>
2017-10-02 14:36:10 -0400

    Revise groups and users again

--- mps/admin.py
+++ mps/admin.py
@@ -18,7 +18,12 @@ class MyUserAdmin(UserAdmin):
     )
 
     def all_groups(self, obj):
-        return '<br>'.join([group.name for group in obj.groups.all().order_by('name')])
+        contents = u''
+        trigger = u''
+        if obj.groups.all().count():
+            contents = u'<br>'.join([group.name for group in obj.groups.all().order_by('name')])
+            trigger = u'<a href="javascript:void(0)" onclick=$("#groups_{0}").toggle()>Show/Hide Groups</a>'.format(obj.pk)
+        return u'{0}<div hidden id="groups_{1}">{2}</div>'.format(trigger, obj.pk, contents)
 
     all_groups.allow_tags = True
 
@@ -35,7 +40,12 @@ class MyGroupAdmin(GroupAdmin):
     )
 
     def all_users(self, obj):
-        return '<br>'.join([' '.join([user.first_name, user.last_name]) for user in obj.user_set.all().order_by('username')])
+        contents = u''
+        trigger = u''
+        if obj.user_set.all().count():
+            contents = u'<br>'.join([' '.join([user.first_name, user.last_name]) for user in obj.user_set.all().order_by('username')])
+            trigger = u'<a href="javascript:void(0)" onclick=$("#users_{0}").toggle()>Show/Hide Users</a>'.format(obj.pk)
+        return u'{0}<div hidden id="users_{1}">{2}</div>'.format(trigger, obj.pk, contents)
 
     all_users.allow_tags = True
 
