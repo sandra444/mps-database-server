150cacf23c24dfd751ffb0247af49100ed27be41
Luke-Malchow-Bergenthal <lmb140@pitt.edu>
2018-02-27 15:23:43 -0500

    Revert sort for now

--- assays/static/assays/reproducibility.js
+++ assays/static/assays/reproducibility.js
@@ -28,7 +28,7 @@ $(document).ready(function () {
                 }
             }
             },
-            { title: "Replica Group", data: '9', type: "numeric" },
+            { title: "Replica Group", data: '9', type: "num" },
             { title: "Organ Model", data: '0' },
             { title: "Cells", data: '1' },
             { title: "Compound<br>Treatment(s)", data: '2' },
