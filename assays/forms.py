1a81d55ae1fc9c957070c0bf430bac5817481f71
Luke-Malchow-Bergenthal <lmb140@pitt.edu>
2018-04-10 15:06:38 -0400

    Exclude tracking from form

--- assays/forms.py
+++ assays/forms.py
@@ -921,7 +921,7 @@ class AssayStudyConfigurationForm(SignOffMixin, ModelFormStripWhiteSpace):
             'media_composition': forms.Textarea(attrs={'cols': 50, 'rows': 3}),
             'hardware_description': forms.Textarea(attrs={'cols': 50, 'rows': 3}),
         }
-        exclude = ('',)
+        exclude = tracking
 
 
 # Forms for plates may become more useful later
