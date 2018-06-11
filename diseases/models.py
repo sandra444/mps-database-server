4a3c311852c7a04c06a64698aba65ce11a56f68b
Quinn Wolter <QKW3@pitt.edu>
2018-06-11 12:04:55 -0400

    Changes to DrugTrials, add FindingTreatment, adjust disease pages and models.

--- diseases/models.py
+++ diseases/models.py
@@ -10,43 +10,27 @@ class Disease(models.Model):
     description = models.TextField(default='', blank=True)
 
     # Everything necessary for the Overview Page
-    overview_state_blurb = models.TextField(default='', blank=True)
+    overview_state_blurb = models.TextField(default='', blank=True, help_text="NOTE: Use HTML tags to create lists.")
     overview_state_image = models.ImageField(upload_to='disease_images', null=True, blank=True)
 
-    # overview_biology_blurb = models.TextField(default='', blank=True)
-    # overview_biology_image = models.ImageField(upload_to='disease_images', null=True, blank=True)
-    #
-    # overview_clinicaldata_blurb = models.TextField(default='', blank=True)
-    # overview_clinicaldata_image = models.ImageField(upload_to='disease_images', null=True, blank=True)
-    # overview_clinicaldata_num_results = models.IntegerField(default=0, null=True, blank=True)
-    # overview_clinicaldata_num_phaseI = models.IntegerField(default=0, null=True, blank=True)
-    # overview_clinicaldata_num_phaseII = models.IntegerField(default=0, null=True, blank=True)
-    # overview_clinicaldata_num_phaseIII = models.IntegerField(default=0, null=True, blank=True)
-    # overview_clinicaldata_num_phaseIV = models.IntegerField(default=0, null=True, blank=True)
-    #
-    # overview_models_blurb = models.TextField(default='', blank=True)
-    # overview_models_image = models.ImageField(upload_to='disease_images', null=True, blank=True)
-    #
-    # # overview_data_blurb = models.TextField(default='', blank=True)
-    # # overview_data_image = models.ImageField(upload_to='disease_images', null=True, blank=True)
-
     # Everything necessary for the Biology Page
-    biology_blurb = models.TextField(default='', blank=True)
+    biology_blurb = models.TextField(default='', blank=True, help_text="NOTE: Use HTML tags to create lists.")
     biology_image = models.ImageField(upload_to='disease_images', null=True, blank=True)
-    biology_kegg_pathway_map = models.ImageField(upload_to='disease_images', null=True, blank=True)
-    biology_kegg_pathway_url = models.CharField(max_length=200, null=True, blank=True)
+    biology_kegg_pathway_map = models.ImageField(upload_to='disease_images', null=True, blank=True, help_text="NOTE: Save and upload the KEGG Pathway Map.")
+    biology_kegg_pathway_url = models.CharField(max_length=200, null=True, blank=True, help_text="Example: http://www.genome.jp/kegg-bin/show_pathway?hsa04932 (Direct link to KEGG Pathway Map)")
+    biology_kegg_url = models.CharField(max_length=200, null=True, blank=True, help_text="Example: http://www.genome.jp/kegg-bin/show_pathway?hsa04932 (Link to KEGG Disease Entry)")
 
     biology_genomic_geo_url = models.CharField(max_length=200, null=True, blank=True)
-    biology_genomic_omim_url = models.CharField(max_length=200, null=True, blank=True)
+    biology_genomic_omim_url = models.CharField(max_length=200, null=True, blank=True, help_text="NOTE: On the OMIM result page for this disease, click the blue 'Gene Map Table' button before copying the URL.")
     biology_genomic_exsnp_url = models.CharField(max_length=200, null=True, blank=True)
     biology_genomic_diseasesorg_url = models.CharField(max_length=200, null=True, blank=True)
 
     # Everything necessary for the Clinical Data Page
-    clinicaldata_blurb = models.TextField(default='', blank=True)
+    clinicaldata_blurb = models.TextField(default='', blank=True, help_text="NOTE: Use HTML tags to create lists.")
     clinicaldata_image = models.ImageField(upload_to='disease_images', null=True, blank=True)
 
     # Everything necessary for the Disease Models Page
-    models_blurb = models.TextField(default='', blank=True)
+    models_blurb = models.TextField(default='', blank=True, help_text="NOTE: Use HTML tags to create lists.")
     models_image = models.ImageField(upload_to='disease_images', null=True, blank=True)
 
     # Everything necessary for the Data Analysis Page
@@ -54,24 +38,3 @@ class Disease(models.Model):
 
     def __unicode__(self):
         return self.name
-
-
-# class DiseaseBiologyOtherResources(models.Model):
-#     disease = models.ForeignKey(Disease, on_delete=models.CASCADE)
-#     name = models.CharField(max_length=200, null=True, blank=True)
-#     description = models.TextField(default='', blank=True)
-#     url = models.CharField(max_length=200, null=True, blank=True)
-#
-#
-# class DiseaseClinicalTrial(models.Model):
-#     disease = models.ForeignKey(Disease, on_delete=models.CASCADE)
-#     trial_id = models.CharField(max_length=200, null=True, blank=True)
-#     trial_name = models.CharField(max_length=200, null=True, blank=True)
-#     phase = models.CharField(max_length=20, null=True, blank=True)
-#     status = models.CharField(max_length=200, null=True, blank=True)
-#     results = models.BooleanField(default=False)
-#     institution = models.CharField(max_length=200, null=True, blank=True)
-#     drugs = models.CharField(max_length=200, null=True, blank=True)
-#
-#     def __unicode__(self):
-#         return "{} - {}".format(self.disease, self.trial_id)
