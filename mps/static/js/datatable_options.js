2a8103b63178974d006404867e69c0e04f66d103
Luke-Malchow-Bergenthal <lmb140@pitt.edu>
2018-12-07 18:02:45 -0500

    Fix fixed headers after toggling sidebar; revise grouping/filtering

--- mps/static/js/datatable_options.js
+++ mps/static/js/datatable_options.js
@@ -80,7 +80,7 @@ $(document).ready(function () {
     });
 
     // Indicates that floating headers need to be refreshed when a toggle-hide-button is clicked
-    $(document).on('click', '.toggle-hide-button', function() {
+    $(document).on('click', '.toggle-hide-button, .toggle_sidebar_button', function() {
         // Recalculate responsive and fixed headers
         setTimeout(function() {
             $($.fn.dataTable.tables(true)).DataTable().responsive.recalc();
