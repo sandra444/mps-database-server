632b3af6c9bdd88345c48a005fcec35768883b65
Luke-Malchow-Bergenthal <lmb140@pitt.edu>
2019-03-26 13:54:12 -0400

    Remove autocollapse of sidebar on resize

--- assays/static/assays/grouping_filtering.js
+++ assays/static/assays/grouping_filtering.js
@@ -689,21 +689,21 @@ $(document).ready(function () {
         window.GROUPING.refresh_wrapper();
     });
 
-    $(window).resize(function() {
-        if ($(window).width() > 768) {
-             $('#page').addClass('pushed');
-             $('#sidebar').addClass('active');
-        }
-        else {
-            $('#page').removeClass('pushed');
-            $('#sidebar').removeClass('active');
-        }
+    // $(window).resize(function() {
+        // if ($(window).width() > 768) {
+        //      $('#page').addClass('pushed');
+        //      $('#sidebar').addClass('active');
+        // }
+        // else {
+        //     $('#page').removeClass('pushed');
+        //     $('#sidebar').removeClass('active');
+        // }
 
         // Adjust datatables
-        setTimeout(function() {
-            $($.fn.dataTable.tables(true)).DataTable().columns.adjust();
-        }, 250);
-    });
+    //     setTimeout(function() {
+    //         $($.fn.dataTable.tables(true)).DataTable().columns.adjust();
+    //     }, 250);
+    // });
 
     if($(window).width() > 768) {
         $('#page').addClass('pushed');
