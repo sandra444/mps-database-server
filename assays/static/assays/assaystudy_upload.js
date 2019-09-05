86e94f8a1c750106fb4fce9f28b0911c95939fd4
sandra444 <sandrakarcher44@gmail.com>
2019-09-05 19:00:01 -0400

    First draft of outline workflow for plate reader data processing tool

--- assays/static/assays/assaystudy_upload.js
+++ assays/static/assays/assaystudy_upload.js
@@ -122,6 +122,8 @@ $(document).ready(function () {
                 processData: false,
                 data: formData,
                 success: function (json) {
+                    //sck
+                    console.log(formData, json)
                     // Stop spinner
                     window.spinner.stop();
 
@@ -169,7 +171,13 @@ $(document).ready(function () {
 
     // Validate file if file selected
     $('#id_bulk_file').change(function() {
-        validate_bulk_file();
+        // sck for raw_plate
+        if ($('input[name=upload_type]:checked').val() === "raw_plate") {
+            // #TODO
+            console.log("run the checks for the plate reader input...talk with Luke about approach so don't get crossed")
+        } else {
+            validate_bulk_file();
+        }
     });
 
     // NOTE: FOR NOW FILTERING WILL BE FORBIDDEN
@@ -188,4 +196,84 @@ $(document).ready(function () {
         get_readouts();
     });
 */
+
+    //sck for raw_plate
+
+    // test data
+    var MOUNTAINS = [
+        {"1":"chip1","2":1,"3":"my treatment 1"},
+        {"1":"chip2","2":2,"3":"my treatment 2"},
+        {"1":"chip3","2":3,"3":"my treatment 3"}
+    ];
+
+    var HEADERMOUNTIANS = ["A","B","C"]
+
+    $('input[type="radio"]').click(function() {
+        var inputValue = $(this).attr("value");
+        var inputName = $(this).attr("name");
+        console.log(inputName)
+        console.log(inputValue)
+        if (inputName == "upload_type")
+            if (inputValue == "raw_plate") {
+                $('.supporting-data').addClass('hidden');
+                $('.mif-c').addClass('hidden');
+                $('.raw-plate').removeClass('hidden');
+                buildTableChips(MOUNTAINS)
+            }
+            else if(inputValue == "supporting_data") {
+                $('.mif-c').addClass('hidden');
+                $('.raw-plate').addClass('hidden');
+                $('.supporting-data').removeClass('hidden');
+
+            } else {
+                $('.raw-plate').addClass('hidden');
+                $('.supporting-data').addClass('hidden');
+                $('.mif-c').removeClass('hidden');
+            }
+        else if (inputName == "plate_map_type_option") {
+            console.log(inputName)
+            if (inputValue == "sample") {
+                $('.sample-times').removeClass('hidden');
+                $('.study-matrices').addClass('hidden');
+            } else {
+                $('.sample-times').addClass('hidden');
+                $('.study-matrices').removeClass('hidden');
+            }
+        } else {
+            var passme = true;
+        }
+    //console.log($('input[name=upload_type]:checked').val())
+    });
+
+
+
+
+
+
+    function buildTableChips(data) {
+        //var table = document.createElement("table");
+        //table.className="gridtable";
+        var thead = document.createElement("thead");
+        var tbody = document.createElement("tbody");
+        var headRow = document.createElement("tr");
+        HEADERMOUNTIANS.forEach(function(el) {
+          var th=document.createElement("th");
+          th.appendChild(document.createTextNode(el));
+          headRow.appendChild(th);
+        });
+        thead.appendChild(headRow);
+        tablecellsselection.appendChild(thead);
+        data.forEach(function(el) {
+          var tr = document.createElement("tr");
+          for (var o in el) {
+            var td = document.createElement("td");
+            td.appendChild(document.createTextNode(el[o]))
+            tr.appendChild(td);
+          }
+          tbody.appendChild(tr);
+        });
+        tablecellsselection.appendChild(tbody);
+        return tablecellsselection;
+    }
+
 });
