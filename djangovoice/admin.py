d3a9746fdf4b9f73c6d31552daf4188e2e3d78e3
Luke-Malchow-Bergenthal <lmb140@pitt.edu>
2019-10-28 15:21:19 -0400

    Revise feedback export

--- djangovoice/admin.py
+++ djangovoice/admin.py
@@ -2,10 +2,27 @@ from django.contrib import admin
 from djangovoice.models import Feedback, Status, Type
 
 from import_export.admin import ImportExportModelAdmin
+from import_export import resources
 
 
 class SlugFieldAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
+
+
+class FeedbackResource(resources.ModelResource):
+    class Meta:
+        model = Feedback
+        fields = (
+            'title',
+            'type__title',
+            'status__title',
+            # 'duplicate',
+            # 'anonymous',
+            'private',
+            'user__username',
+            'description',
+            # 'email'
+        )
 
 
 class FeedbackAdmin(ImportExportModelAdmin):
@@ -28,6 +45,12 @@ class FeedbackAdmin(ImportExportModelAdmin):
     #     'user', 'email']
     # list_filter = ['type', 'status', 'anonymous', 'private']
     # list_editable = ['type', 'status', 'anonymous', 'private']
+
+    resource_class = FeedbackResource
+
+    def get_queryset(self, request):
+        queryset = super(FeedbackAdmin, self).get_queryset(request)
+        return queryset.prefetch_related('user')
 
 
 admin.site.register(Feedback, FeedbackAdmin)
