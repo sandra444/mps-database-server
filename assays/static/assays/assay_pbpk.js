f6b81d696945320303bcbfffe961663a24e05696
Quinn K Wolter <QuinnKWolter@gmail.com>
2020-05-11 12:53:46 -0400

    Implement Luke's suggestion to fix Firefox regex issue.

--- assays/static/assays/assay_pbpk.js
+++ assays/static/assays/assay_pbpk.js
@@ -586,7 +586,7 @@ $(document).ready(function () {
     });
 
     function numberWithCommas(x) {
-        return x.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
+        return x.toLocaleString('en-US');
     }
 
     $('#button-dosing').click(function() {
