cdcb3a216e04d957275f58008c9582d1b24332f9
Luke-Malchow-Bergenthal <lmb140@pitt.edu>
2020-09-22 18:33:32 -0400

    Add webinar banner

--- mps/static/js/index.js
+++ mps/static/js/index.js
@@ -39,4 +39,14 @@ $(function() {
         },
         minLength: 2
     });
+
+    // Hard coded at the moment
+    if (!localStorage.getItem('hide_banner')) {
+        $('#webinar_banner').show('slow');
+    }
+
+    $('#close_webinar_banner').click(function() {
+        $('#webinar_banner').hide('slow');
+        localStorage.setItem('hide_banner', 'TRUE');
+    });
 });
