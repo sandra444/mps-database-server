46cf1274e47ea6772a84ab8f7304ae8431460711
sandra444 <sandrakarcher44@gmail.com>
2020-10-15 12:37:27 -0400

    still a mess, jumping offset

--- mps/static/js/help.js
+++ mps/static/js/help.js
@@ -1,30 +1,30 @@
 $(document).ready(function () {
-    let offset = 110;
+    var offset = 0;
+    var help_offset = 0;
+    var global_true_if_all_are_open = false;
 
-    let global_true_if_all_are_open = false;
-
-    // let initial_hash = window.location.hash;
-    let initial_hash = '#help_heatmap_bioactivities';
-    animate_scroll_hash(initial_hash);
+    var initial_hash = window.location.hash;
+    initial_hash = '#video_demo_w1';
+    if (initial_hash) {
+        animate_scroll_hash(initial_hash);
+    }
 
     function animate_scroll_hash(this_hash) {
-        console.log("ih "+this_hash)
-        if (this_hash) {
-            // if the anchor is NOT on the page, do not cause and error in the console
-            // this error causes the glossary NOT to display!!!
-            try {
-                $('html, body').animate({
-                    scrollTop: $(this_hash).offset().top - offset
-                }, 500);
-                $(this_hash).find('button').next().first().css("display", "block");
-            } catch {
-            }
+        // if the anchor is NOT on the page, do not cause and error in the console
+        // this error causes the glossary NOT to display!!!
+        if ($(initial_hash).length)
+        {
+            $('html, body').animate({
+                scrollTop: $(this_hash).offset().top - help_offset
+            }, 500);
+            $(this_hash).find('button').next().first().css("display", "block");
         }
     }
 
     $(".help-collapsible").click(function() {
-        let content = this.nextElementSibling;
-        let parent_id = '';
+        // console.log("help-collapsible")
+        var content = this.nextElementSibling;
+        var parent_id = '';
         try {
             parent_id = $(this).parent().attr('id');
         } catch {
@@ -37,11 +37,11 @@ $(document).ready(function () {
     $("#expand_all").click(function(){
         //https://stackoverflow.com/questions/24844566/click-all-buttons-on-page
         // Get all buttons with the name and store in a NodeList called 'buttons'
-        let buttons = document.getElementsByName('help_button');
+        var buttons = document.getElementsByName('help_button');
         // Loop through and change display of the content
-        for (let i = 0; i <= buttons.length; i++) {
+        for (var i = 0; i <= buttons.length; i++) {
             try {
-                let content = buttons[i].nextElementSibling;
+                var content = buttons[i].nextElementSibling;
                 change_display('expand_all', content, 'expand');
             } catch {
             }
@@ -49,11 +49,11 @@ $(document).ready(function () {
         global_true_if_all_are_open = true;
     })
     $("#close_all").click(function(){
-        let buttons = document.getElementsByName('help_button');
+        var buttons = document.getElementsByName('help_button');
         // Loop through and change display of the content
-        for (let i = 0; i <= buttons.length; i++) {
+        for (var i = 0; i <= buttons.length; i++) {
             try {
-                let content = buttons[i].nextElementSibling;
+                var content = buttons[i].nextElementSibling;
                 change_display('close_all', content, 'close');
             } catch {
             }
@@ -62,7 +62,7 @@ $(document).ready(function () {
     })
 
     function change_display(this_checker, content, what_doing) {
-        // console.log(this_checker,content,what_doing)
+        console.log("dis "+what_doing)
         if (what_doing === 'toggle') {
             if ($(content).css("display") != "none") {
                 $(content).css("display", "none");
@@ -77,6 +77,24 @@ $(document).ready(function () {
         }
     }
 
+    $(document).on('click', '.searchButtonClickText_h', function (event) {
+        if (!global_true_if_all_are_open) {
+            var buttons = document.getElementsByName('help_button');
+            for (var i = 0; i <= buttons.length; i++) {
+                try {
+                    var content = buttons[i].nextElementSibling;
+                    change_display('expand_all', content, 'expand');
+                } catch {
+                }
+            }
+        }
+        $(".highlighted").removeClass("highlighted").removeClass("match");
+        if (!searchAndHighlight($('.textSearchvalue_h').val())) {
+            alert("No results found (or search box is empty)");
+        }
+
+    });
+
     // https://www.aspforums.net/Threads/211834/How-to-search-text-on-web-page-similar-to-CTRL-F-using-jQuery/
     // http://jsfiddle.net/wjLmx/23/
     function searchAndHighlight(searchTerm, selector) {
@@ -86,28 +104,74 @@ $(document).ready(function () {
             // i = With this flag the search is case-insensitive: no difference between A and a (see the example below).
             // g = With this flag the search looks for all matches, without it – only the first match is returned.
             var selector = selector || "#realTimeContents"; //use body as selector if none provided
-            var searchTermRegEx = new RegExp(searchTerm, "g");
+            var searchTermRegEx = new RegExp(searchTerm, "ig");
             var matches = $(selector).text().match(searchTermRegEx);
+            // the matches list is case insensitive
 
             if (matches != null && matches.length > 0) {
                 //Remove old search highlights
                 $('.highlighted').removeClass('highlighted');
-
                 //Remove the previous matches
                 $span = $('#realTimeContents span');
                 $span.replaceWith($span.html());
 
                 if (searchTerm === "&") {
                     searchTerm = "&amp;";
-                    searchTermRegEx = new RegExp(searchTerm, "g");
+                    searchTermRegEx = new RegExp(searchTerm, "ig");
                 }
-
                 $(selector).html($(selector).html().replace(searchTermRegEx, "<span class='match'>" + searchTerm + "</span>"));
+
+                console.log("here")
+                $('.match').each(function(index, currentElement) {
+
+                    console.log(index)
+                    console.log("currentElement "+currentElement)
+                    console.log("currentElement.innerHTML "+currentElement.innerHTML)
+                    console.log("matches[index] "+matches[index])
+                    //currentElement.replace(currentElement.innerHTML, "<span class='match'>" + matches[index] + "</span>"));
+                });
+                console.log("here2")
+
+
+                // var allListElements = $(".match");
+                // console.log("e "+allListElements)
+                // m = 0;
+                // allListElements.each(function() {
+                //     console.log("m "+m)
+                //     console.log(this)
+                //     $(this).replace("<span class='match'>" + searchTerm + "</span>", "<span class='match'>" + match[m] + "</span>");
+                //     m++;
+                // });
+
+                // var unique_matches = matches.filter(function(itm, i, a) {
+                //     return i == matches.indexOf(itm);
+                // });
+                //
+                // for (var j = 0; j < unique_matches.length; j++) {
+                //     if (j == 1) {
+                //         console.log("j " + j)
+                //         console.log("matches[j] " + unique_matches[j])
+                //         searchTerm = unique_matches[j];
+                //         if (searchTerm === "&") {
+                //             searchTerm = "&amp;";
+                //             searchTermRegEx = new RegExp(searchTerm, "g");
+                //             console.log("searchTermRegEx " + searchTermRegEx)
+                //         } else {
+                //             searchTermRegEx = new RegExp(searchTerm, "g");
+                //             console.log("searchTermRegEx " + searchTermRegEx)
+                //         }
+                //         $(selector).html($(selector).html().replace(searchTermRegEx, "<span class='match'>" + matches[j] + "</span>"));
+                //
+                //     }
+                // }
+
+                // console.log("fire 5")
                 $('.match:first').addClass('highlighted');
 
                 var i = 0;
 
                 $('.next_h').off('click').on('click', function () {
+                    // console.log("fire 6")
                     i++;
                     if (i >= $('.match').length) {
                         i = 0;
@@ -117,7 +181,7 @@ $(document).ready(function () {
                     $('.match').eq(i).addClass('highlighted');
                     // **$('.highlighted').attr('id', 'id_high');
 
-                    // let idArray = [];
+                    // var idArray = [];
                     // $('.highlighted').each(function () {
                     //     HANDY to get the text back from a nodeValue
                     //     console.log("i "+this.firstChild.nodeValue)
@@ -125,32 +189,37 @@ $(document).ready(function () {
                     // });
 
                     // **this works, but will need a lot of stop points to use it
-                    // **let id_parents = $('#id_high').parents('.stop-point').attr('id');
-                    // **let this_hash = '#' + id_parents;
+                    // **var id_parents = $('#id_high').parents('.stop-point').attr('id');
+                    // **var this_hash = '#' + id_parents;
                     // **animate_scroll_hash(this_hash, 'search');
 
+                    // console.log("animate next")
                     $('html, body').animate({
-                        scrollTop: $('.highlighted:visible:first').offset().top - (offset+30)
+                        scrollTop: $('.highlighted:visible:first').offset().top - (help_offset)
                     }, 400);
 
                 });
 
                 $('.previous_h').off('click').on('click', function () {
+                    // console.log("fire 7")
                     i--;
                     if (i < 0) {
                         i = $('.match').length - 1;
                     }
                     $('.match').removeClass('highlighted');
                     $('.match').eq(i).addClass('highlighted');
+
+                    // console.log("animate previous")
                     $('html, body').animate({
-                        scrollTop: $('.highlighted:visible:first').offset().top - (offset+30)
+                        scrollTop: $('.highlighted:visible:first').offset().top - (help_offset)
                     }, 400);
                 });
 
                 // when the search is clicked - finds the first occurance
                 if ($('.highlighted:first').length) {
+                    // console.log("animate search first")
                     //if match found, scroll to where the first one appears
-                    $(window).scrollTop($('.highlighted:first').position().top - offset);
+                    $(window).scrollTop($('.highlighted:first').position().top - (help_offset));
                 }
                 return true;
             }
@@ -158,29 +227,12 @@ $(document).ready(function () {
         return false;
     }
 
-    $(document).on('click', '.searchButtonClickText_h', function (event) {
-        if (!global_true_if_all_are_open) {
-            let buttons = document.getElementsByName('help_button');
-            for (let i = 0; i <= buttons.length; i++) {
-                try {
-                    let content = buttons[i].nextElementSibling;
-                    change_display('expand_all', content, 'expand');
-                } catch {
-                }
-            }
-        }
-        $(".highlighted").removeClass("highlighted").removeClass("match");
-        if (!searchAndHighlight($('.textSearchvalue_h').val())) {
-            // alert("No results found");
-        }
-
-    });
-
     $('a').not("[href*='/']").click(function(event) {
         event.preventDefault();
+        // console.log("animate a")
         if ($($(this).attr('href'))[0]) {
             $('html, body').animate({
-                scrollTop: $($(this).attr('href')).offset().top - offset
+                scrollTop: $($(this).attr('href')).offset().top - help_offset
             }, 500);
             $($(this).attr('href')).find('button').next().first().css("display", "block");
         }
@@ -236,4 +288,5 @@ $(document).ready(function () {
         _alphabetSearch = $(this).attr('data-letter');
         glossary_table.draw();
     });
+
 });
