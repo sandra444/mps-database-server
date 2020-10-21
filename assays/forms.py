b95871f03d65695471444c3c937f80986f22f9f7
Luke-Malchow-Bergenthal <lmb140@pitt.edu>
2020-10-21 12:55:41 -0400

    Fix flag for Matrix Item

--- assays/forms.py
+++ assays/forms.py
@@ -3156,7 +3156,7 @@ class AssayMatrixItemForm(SetupFormsMixin, SignOffMixin, BootstrapForm):
             'notebook',
             'notebook_page',
             'notes'
-        )
+        ) + flag_group
 
     def __init__(self, *args, **kwargs):
         super(AssayMatrixItemForm, self).__init__(*args, **kwargs)
