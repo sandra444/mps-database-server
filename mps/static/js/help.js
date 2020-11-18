40047162e95d111f5a0dcf5c74139fdccacbbe55
sandra444 <sandrakarcher44@gmail.com>
2020-11-18 11:56:12 -0500

    update data sharing

--- mps/static/js/help.js
+++ mps/static/js/help.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
 
     var glossary_spans_on = true;
-    //after done editing, do this
+    //after done editing, do this **change
     //strip_the_glossary_spans()
 
     var initial_hash = window.location.hash;
@@ -378,7 +378,7 @@ $(document).ready(function () {
             '#assays-assaystudy-index': '#help_overview_organization',
         };
 
-        var set_hash_default = '#search_term';
+        var set_hash_default = '#search_help_page_section';
         var initial_hash_help = set_hash_default;
         if (!initial_hash || initial_hash == '#None') {
             initial_hash_help = set_hash_default;
@@ -434,6 +434,11 @@ $(document).ready(function () {
         // if the anchor is NOT on the page, do not cause and error in the console
         // this error causes the glossary NOT to display!!!
         // console.log('animate anchor ',anchor)
+        if (anchor === '#search_help_page_section') {
+            $('#search_help_page_section').removeClass('hidden');
+        } else {
+            $('#search_help_page_section').addClass('hidden');
+        }
 
         if ($(anchor).length)
         {
